eeb8291be79ac1707e811d54c0cb96d981dabacb
Sugi-ai <suhdei90@gmail.com>
2025-08-06 22:31:41 +0800

    todolist Delete button nemsen.
    gol ni map bolon filter ashiglaj element ees gadna index bas i  nemsen.

--- toDoList.js
+++ toDoList.js
@@ -5,7 +5,7 @@
 //   listData.push(value);
 // };
 
-const listData = [];
+let listData = [];
 const ul = document.querySelector("ul");
 const body = document.getElementsByTagName("body")[0];
 const input = document.createElement("input");
@@ -15,9 +15,19 @@ button.innerText = "Click me";
 
 const render = () => {
   ul.innerHTML = "";
-  listData.map((element) => {
+  listData.map((element, index) => {
     const li = document.createElement("li");
+    const removeBtn = document.createElement("button");
+    removeBtn.innerText = "Delete";
+    removeBtn.addEventListener("click", () => {
+      const newListData = listData.filter((el, i) => {
+        return i !== index;
+      });
+      listData = newListData;
+      render();
+    });
     li.innerText = element;
+    li.appendChild(removeBtn);
     ul.appendChild(li);
   });
 };
